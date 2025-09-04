307c917ca348a4dc3106df0c9bf257105aa136d6
Andrés Gallo <111935404+Pein011z@users.noreply.github.com>
2025-09-03 20:54:55 -0500

    Add files via upload
    UPDATE applicattion.properties y pom.xml

---
+++ conjunto-mamón/src/main/kotlin/com/example/conjunto_mamon/ConjuntoMamonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.conjunto_mamon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConjuntoMamonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConjuntoMamonApplication>(*args)
+}
---
+++ conjunto-mamón/src/test/kotlin/com/example/conjunto_mamon/ConjuntoMamonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.conjunto_mamon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConjuntoMamonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
